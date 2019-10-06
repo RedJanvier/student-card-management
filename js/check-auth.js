c81ebbc4e0bddb1284dff6e3e9077ee9fa27c330
RedJanvier <janvierntwali@gmail.com>
2019-10-06 21:35:14 +0200

    [check-auth.js] fetched cards from firebase and listed them in table

--- js/check-auth.js
+++ js/check-auth.js
@@ -1,15 +1,27 @@
 const logout_btn = document.querySelector('.logout');
+const cards_list = document.querySelector('.cards-list');
 
+console.log('[check-auth.js] working...', );
 logout_btn.addEventListener('click', e => {
     firebase.auth().signOut();
     // location.href = '/student-card-management/management-login.html';
     location.href = '/management-login.html';
 });
-// firebase.firestore().collection('student-cards')
-// .get()
-// .orderBy('created_at')
-// .then(cards => {
-//     console.log(cards);
-//     localStorage.setItem(JSON.stringify(cards));
-// })
-// .catch(console.log);
+
+db.collection('student-cards')
+.get()
+.then(snaps => {
+    let html;
+    snaps.forEach(snap => {
+        const card = snap.data();
+        html = html + `
+            <tr class = "student-card">
+                <th> ${card.reg_number} </th>
+                <td> ${card.firstname} ${card.lastname} </td>
+                <td> ${card.created_at} </td>
+            </tr>
+        `;
+    });
+    cards_list.innerHTML = html;
+})
+.catch(console.log);
