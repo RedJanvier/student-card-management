b5f852dfe8cf0b2a5fb84076263dbb9612136bf0
RedJanvier <janvierntwali@gmail.com>
2020-04-05 16:32:44 +0200

    added prettier

--- public/js/check-auth.js
+++ public/js/check-auth.js
@@ -1,26 +1,27 @@
 const logout_btn = document.querySelector('.logout');
 const cards_list = document.querySelector('.cards-list');
 
-const delete_card = number => {
-    console.log(number);
-}
+const delete_card = (number) => {
+  console.log(number);
+};
 
-logout_btn.addEventListener('click', e => {
-
-    firebase.auth().signOut();
-    localStorage.clear();
-    location.href = '/student-card-management/management-login.html';
-    // location.href = '/management-login.html';
+logout_btn.addEventListener('click', (e) => {
+  firebase.auth().signOut();
+  localStorage.clear();
+  location.href = '/student-card-management/management-login.html';
+  // location.href = '/management-login.html';
 });
 
 db.collection('student-cards')
-.onSnapshot(snaps => {
+  .onSnapshot((snaps) => {
     let html;
     const cards = [];
-    snaps.forEach(snap => {
-        const card = snap.data();
-        cards.push(card);
-        html = html + `
+    snaps.forEach((snap) => {
+      const card = snap.data();
+      cards.push(card);
+      html =
+        html +
+        `
             <tr class = "student-card">
                 <th> ${card.reg_number} </th>
                 <td> <a href="/student-card-management/personal.html?id=${card.reg_number}">${card.firstname} ${card.lastname}</a> </td>
@@ -31,5 +32,5 @@ db.collection('student-cards')
     });
     localStorage.setItem('cards', JSON.stringify(cards));
     cards_list.innerHTML = html;
-})
-.catch(console.log);
+  })
+  .catch(console.log);
